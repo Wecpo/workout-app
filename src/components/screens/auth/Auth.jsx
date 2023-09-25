86058093f811b79c5ecc32e7a35729e15cf801e4
Senin Maksim <wecpo@yandex.ru>
2023-09-25 18:21:31 +0300

    developed auth with backend

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -5,26 +5,40 @@ import Field from '../../ui/field/Field'
 import Loader from '../../ui/loader/Loader'
 import { useState } from 'react'
 import styles from './Auth.module.scss'
+import AuthService from '../../../services/authService'
+import { useMutation } from '@tanstack/react-query'
 
 const Auth = () => {
-	const [type, setType] = useState('auth')
+	const [type, setType] = useState('login')
 	const {
 		register,
 		handleSubmit,
-		formState: { errors }
+		formState: { errors },
+		reset
 	} = useForm({
 		mode: 'onChange'
 	})
 
+	const { mutate, isLoading } = useMutation(
+		['auth'],
+		({ email, password }) => AuthService.main(email, password, type),
+		{
+			onSuccess: data => {
+				alert('success')
+				reset()
+			}
+		}
+	)
+
 	const onSubmit = data => {
-		console.log(data, errors, type)
+		mutate(data)
 	}
 
 	return (
 		<>
 			<Layout heading='Sign in' bgImage='/images/auth-bg.png' />
 			<div className='wrapper-inner-page'>
-				{/* {isLoading || isLoadingAuth && <Loader />} */}
+				{isLoading && <Loader />}
 				<form onSubmit={handleSubmit(onSubmit)}>
 					<Field
 						error={errors.email?.message}
@@ -43,8 +57,8 @@ const Auth = () => {
 						placeholder='Enter password'
 					/>
 					<div className={styles.wrapperButtons}>
-						<Button clickHandler={() => setType('auth')}>Sign in</Button>
-						<Button clickHandler={() => setType('reg')}>Sign up</Button>
+						<Button clickHandler={() => setType('login')}>Sign in</Button>
+						<Button clickHandler={() => setType('register')}>Sign up</Button>
 					</div>
 				</form>
 			</div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './components/screens/home/Home.jsx'
 import './assets/styles/index.scss'
+import {
+	useQuery,
+	useMutation,
+	useQueryClient,
+	QueryClient,
+	QueryClientProvider
+} from '@tanstack/react-query'
 import Router from './routes/Router.jsx'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Router />
+		<QueryClientProvider client={queryClient}>
+			<Router />
+		</QueryClientProvider>
 	</React.StrictMode>
 )
